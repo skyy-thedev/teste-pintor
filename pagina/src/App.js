f860655c908416f1e1e7ca2c1e3ab8e16fcea44a
Skyy the dev <skyzeduardo@gmail.com>
2026-01-28 17:54:33 -0300

    troca de react router para react padrao

---
+++ pagina/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Cabeçalho</header>
+      <header className="App-container">
+        <h1>Hello World</h1>
+      </header>
+
+      <div class="container">
+          <h1>Transforme Seu Espaço com Pintura Profissional 🎨</h1>
+      </div>
+
+      <div class="container">
+        <p>Seu ambiente merece uma nova vida — e a pintura certa faz toda a diferença.</p>
+        <h2>Trabalhamos com pintura residencial, comercial e decorativa, entregando acabamento impecável, cores vibrantes e total cuidado com cada detalhe.</h2>
+      </div>
+
+      <div class="container-cards">
+        <div class="card">
+            <p>✅ Pintores especializados e materiais de alta qualidade</p>
+        </div>
+        <div class="card">
+            <p>✅ Pontualidade e limpeza em todas as etapas</p>
+        </div>
+        <div class="card">
+            <p>✅ Orçamento gratuito e sem compromisso</p>
+        </div>
+        <div class="card">
+            <p>✅ Serviço rápido e garantido</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
